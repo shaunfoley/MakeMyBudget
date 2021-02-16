c26474c5b465edf446b8900fa0212dcbc664a305
Shaun Foley <shaunfoley28@windowslive.com>
2021-02-16 20:48:42 +1300

    Built class structure for finances from existing Excel Spreadsheet to XML

---
+++ MakeMyBudget/BudgetCore/Class1.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace BudgetCore
+{
+    public class Account
+    {
+        [JsonProperty("@name")]
+        public string Name { get; set; }
+
+        [JsonProperty("balance")]
+        public string Balance { get; set; }
+    }
+
+    public class Accounts
+    {
+        [JsonProperty("account")]
+        public List<Account> Account { get; set; }
+    }
+
+    public class Payment
+    {
+        [JsonProperty("@name")]
+        public string Name { get; set; }
+
+        [JsonProperty("frequency")]
+        public string Frequency { get; set; }
+
+        [JsonProperty("day")]
+        public string Day { get; set; }
+
+        [JsonProperty("to")]
+        public string To { get; set; }
+
+        [JsonProperty("amount")]
+        public string Amount { get; set; }
+
+        [JsonProperty("from")]
+        public string From { get; set; }
+    }
+
+    public class Payments
+    {
+        [JsonProperty("payment")]
+        public List<Payment> Payment { get; set; }
+    }
+
+    public class Data
+    {
+        [JsonProperty("@xmlns:xsi")]
+        public string XmlnsXsi { get; set; }
+
+        [JsonProperty("accounts")]
+        public Accounts Accounts { get; set; }
+
+        [JsonProperty("payments")]
+        public Payments Payments { get; set; }
+    }
+
+    public class Root
+    {
+        [JsonProperty("data")]
+        public Data Data { get; set; }
+    }
+
+
+}
---
+++ MakeMyBudget/MakeMyBudget/Program.cs
@@ -0,0 +1,23 @@
+﻿using BudgetCore;
+using Newtonsoft.Json;
+using System;
+using System.Xml;
+
+namespace MakeMyBudget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string xml = System.IO.File.ReadAllText(@"D:\Downloads\FinanceData.xml");
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xml);
+
+            string json = JsonConvert.SerializeXmlNode(doc);
+
+            Root ConvertedJson = JsonConvert.DeserializeObject<Root>(json);
+
+            Console.WriteLine(JsonConvert.SerializeObject(ConvertedJson));
+        }
+    }
+}
